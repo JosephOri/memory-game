e43a6a61496f7a8c813686b00700dc0191531601
Ori <ori100no@gmail.com>
2023-05-19 12:14:51 +0300

    made a method to display the cards and a score line, I also need to add an event listener to the click card method

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 var cards = [];
+var score = 0;
+var scoreDiv = document.getElementById("score");
+
 document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    var form = document.getElementById("myForm");
-    let submittedNumber = parseInt(form.inputNumber.value);
-    removeAllElement(form);
-    document.appendChild
-  });
+  event.preventDefault();
   
+  var form = document.getElementById("myForm");
+  var submittedNumber = parseInt(form.inputNumber.value);
+  
+  if (submittedNumber > 30) {
+    alert("Input must be less than 30");
+    return;
+  }
+  
+  removeAllElement(form);
+  scoreDiv.textContent = "score: "+score;
 
-function removeAllElement(form) {
-    while (form.firstChild) {
-        form.removeChild(form.firstChild);
+  var memoryCardsDiv = document.getElementById("memory-cards");
+  
+  for (var i = 0; i < submittedNumber; i++) {
+    for (var j = 0; j < 2; j++) {
+      var newCard = document.createElement("div");
+      newCard.classList.add("memory-card");
+      
+      var cardBackUrl = "cards/card-back.png";
+      newCard.style.backgroundImage = "url('" + cardBackUrl + "')";
+     // newCard.addEventListener("click", );
+      
+      cards.push(newCard);
+      
+      memoryCardsDiv.appendChild(newCard);
     }
-    form.remove();
-}
+  }
+});
 
+function removeAllElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+  element.remove();
+}
